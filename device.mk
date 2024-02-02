da9f8295538a06a01564a3ec5ba966fe480b79b5
johnny-lawer <gnogueira3163@gmail.com>
2024-02-02 17:19:06 -0300

    fix fstab and add suporty dynamic partition and other stuff

--- device.mk
+++ device.mk
@@ -6,3 +6,10 @@
 #
 
 LOCAL_PATH := device/samsung/a34x
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
