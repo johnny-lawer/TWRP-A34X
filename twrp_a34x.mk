61387538deeb5e27389e903664ad5f482c2edf50
johnny-lawer <gnogueira3163@gmail.com>
2024-02-07 09:43:36 -0300

    twrp name

--- twrp_a34x.mk
+++ twrp_a34x.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a34x device
 $(call inherit-product, device/samsung/a34x/device.mk)
 
 PRODUCT_DEVICE := a34x
-PRODUCT_NAME := omni_a34x
+PRODUCT_NAME := twrp_a34x
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A346B
 PRODUCT_MANUFACTURER := samsung
